a79f26a48e326da88ae02ce07c7b0f6077b033af
Victor Lopes <victor.cabrera0209@gmail.com>
2021-08-08 19:42:51 -0300

    Try to solve 6 and 7 requirement

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 CREATE VIEW faturamento_atual AS
-SELECT ROUND(MIN(p.valor_plano), 2) AS 'faturamento_minimo',
-ROUND(MAX(p.valor_plano), 2) AS 'faturamento_maximo',
+SELECT MIN(p.valor_plano) AS 'faturamento_minimo',
+MAX(p.valor_plano) AS 'faturamento_maximo',
 ROUND(AVG(p.valor_plano), 2) AS 'faturamento_medio',
 ROUND(SUM(p.valor_plano), 2) AS 'faturamento_total'
 FROM SpotifyClone.planos p
--- desafio7.sql
+++ desafio7.sql
@@ -3,5 +3,5 @@ SELECT a.artista AS 'artista', l.album AS album, COUNT(ua.usuario_id) AS 'seguid
 FROM SpotifyClone.usuarios_artistas ua
 INNER JOIN SpotifyClone.artistas a ON ua.artista_id = a.artista_id
 INNER JOIN SpotifyClone.albuns l ON ua.artista_id = l.artista_id
-GROUP BY artista, album
-ORDER BY seguidores DESC, artista;
+GROUP BY a.artista, l.album
+ORDER BY COUNT(ua.usuario_id) DESC, a.artista;
